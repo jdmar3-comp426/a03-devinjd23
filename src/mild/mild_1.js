d081f994130445b5c979ece7edd3bd413424a9c3
Devin Dwyer <devind9621@gmail.com>
2021-10-13 23:31:46 -0400

    sumToString

--- src/mild/mild_1.js
+++ src/mild/mild_1.js
@@ -9,9 +9,10 @@
  * see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
  */
 export function sumToString(a, b) {
-
+    return a + b;
 }
 
+console.log(sumToString(5, 7));
 
 /**
  *
