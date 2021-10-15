0ed2942aa6e234827cffb217083f83d8e6e472d9
Devin Dwyer <devind9621@gmail.com>
2021-10-15 14:08:34 -0400

    implementation of test file

---
+++ src/test.js
@@ -0,0 +1,8 @@
+import {sumToString, getIncreasingArray, maxAndMin, countArray} from "./mild/mild_1.js";
+
+// mild_1.js tests
+console.log(sumToString(3, 4));
+console.log(getIncreasingArray(3, 9));
+console.log(maxAndMin([4, 23, 7, 9, 1]));
+
+// mild_2.js tests
