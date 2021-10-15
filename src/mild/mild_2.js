3ac60049d494e7623b41f55ce13816b8c1e15642
Devin Dwyer <devind9621@gmail.com>
2021-10-15 14:12:52 -0400

    identifyVariable function

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -6,7 +6,8 @@
  * returns: { type: 'number', value: 4 }
  */
 export function identifyVariable(variable) {
-
+   let type = typeof(variable);
+   return {type, variable};
 }
 
 
--- src/test.js
+++ src/test.js
@@ -1,4 +1,5 @@
 import {sumToString, getIncreasingArray, maxAndMin, countArray} from "./mild/mild_1.js";
+import {identifyVariable, identifyArray, removeKey, removeKeyNonDestructive, removeKeys} from "./mild/mild_2.js";
 
 // mild_1.js tests
 console.log(sumToString(3, 4));
@@ -6,3 +7,8 @@ console.log(getIncreasingArray(3, 9));
 console.log(maxAndMin([4, 23, 7, 9, 1]));
 
 // mild_2.js tests
+console.log(identifyVariable(2))
+console.log(identifyVariable(true))
+console.log(identifyVariable("hey"))
+console.log(identifyVariable([3, 4]))
+console.log(identifyVariable({"key": 2}))
