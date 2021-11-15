4e3c60de22def09e1531f288a4e8573afd55cd77
Devin Dwyer <devind9621@gmail.com>
2021-11-15 16:46:42 -0500

    return value on repeat function

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -17,9 +17,8 @@
 export const repeat = (fn, n, ...params) => {
     let funcReturns = [];
     for (let i = 0; i < n; i++) {
-        funcReturns.push(fn(params));
+        return fn(params);
     }
-    return funcReturns;
 };
 
 
