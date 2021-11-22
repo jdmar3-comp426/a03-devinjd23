5bf7a0dd6ebb31df4886e4f3782939e415705aa2
Devin Dwyer <devind9621@gmail.com>
2021-11-21 22:36:41 -0500

    final spicy nine attempt

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -124,7 +124,12 @@ export const everyEven = (arr, test) => {
  *    someEven([0, 0, 0, 0, 0], x => x === 0)  <--  returns true
  */
 export const someEven = (arr, test) => {
-
+    for (let i = 0; i < arr.length; i+=2) {
+        if (test(arr[i])) {
+            return true;
+        }
+    }
+    return false;
 };
 
 
@@ -150,7 +155,19 @@ export const someEven = (arr, test) => {
  *       -->  { pass: [1, 5, 31], fail: [90] }
  */
 export const filter = (arr, test) => {
-
+    let failArr = [];
+    let passArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (test(arr[i])) {
+            passArr.push(arr[i]);
+        } else {
+            failArr.push(arr[i]);
+        }
+    }
+    return {
+        pass: passArr,
+        fail: failArr        
+    };
 };
 
 
@@ -160,7 +177,7 @@ export const filter = (arr, test) => {
  *   odd numbers. Use the "everyEven" function in this function.
  */
 export const allEvensAreOdd = (arr) => {
-
+    return everyEven(arr, x => x % 2 === 1);
 };
 
 
@@ -170,7 +187,7 @@ export const allEvensAreOdd = (arr) => {
  *   array is an odd number. Use the "someEven" function in this function.
  */
 export const anEvenIsOdd = (arr) => {
-
+    return someEven(arr, x => x % 2 === 1);
 };
 
 
@@ -181,5 +198,10 @@ export const anEvenIsOdd = (arr) => {
  *   pass the test. You must use the filter function.
  */
 export const hasExactly = (arr, test, n) => {
-
+    let obj = filter(arr, test);
+    if (obj.pass.length == n) {
+        return true;
+    } else {
+        return false;
+    }
 };
